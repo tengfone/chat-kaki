df9b427a317b4b7b75d24624d948f01866d3d1ea
tengfone <phangtf@gmail.com>
2023-09-04 00:19:32 +0800

    COmpleted kaki card

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,8 +1,35 @@
 import { Categories } from "@/components/categories";
+import { Kakis } from "@/components/kakis";
 import { SearchInput } from "@/components/search-input"
 import prismadb from "@/lib/prismadb" // import this as a global var to access the db
 
-const RootPage = async () => {
+interface RootPageProps {
+    searchParams: { // convention nextJS
+        categoryId: string;
+        name: string;
+    }
+}
+
+const RootPage = async ({ searchParams }: RootPageProps) => {
+
+    const data = await prismadb.kaki.findMany({
+        where: {
+            categoryId: searchParams.categoryId,
+            name: {
+                search: searchParams.name
+            }
+        },
+        orderBy: {
+            createdAt: "desc"
+        },
+        include: {
+            _count: {
+                select: {
+                    messages: true
+                }
+            }
+        }
+    })
 
     const categories = await prismadb.category.findMany();
 
@@ -10,6 +37,7 @@ const RootPage = async () => {
         <div className="h-full p-4 space-y-2">
             <SearchInput />
             <Categories data={categories} />
+            <Kakis data={data} />
         </div>
     )
 }
